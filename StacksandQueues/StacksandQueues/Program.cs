07ef80873701a410fcdd22f88ea19cd1c5e5ce4f
Daneesh <mustafadaneesh@gmail.com>
2020-10-28 19:59:42 +0530

    [Daneesh] Add : Ability to Peek and Pop from Stack till it is Empty

--- StacksandQueues/StacksandQueues/Program.cs
+++ StacksandQueues/StacksandQueues/Program.cs
@@ -6,12 +6,16 @@ namespace StacksandQueues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stack !");
+            Console.WriteLine("Welcome to Stack !!");
             Console.WriteLine("\n");
             Stack stack = new Stack();
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.IsEmpty();
             stack.Display();
         }
     }
--- StacksandQueues/StacksandQueues/Stack.cs
+++ StacksandQueues/StacksandQueues/Stack.cs
@@ -22,6 +22,34 @@ namespace StacksandQueues
                 top = node;
             }
         }
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Empty Stack !!");
+                return;
+            }
+            Console.WriteLine("TOP of Stack ->" + this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Oops ! Empty Stack.");
+                return;
+            }
+            Console.WriteLine("Deleted Value : "+ this.top.data);
+            Console.WriteLine("\n");
+            this.top = this.top.next;
+        }
+        public void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
         public void Display()
         {
             if (top == null)
